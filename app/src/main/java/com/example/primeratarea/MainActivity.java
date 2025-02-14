c158da4581a198546591df30f75586efa8250cee
Pablo Bueno <pablobndq@gmail.com>
2025-02-14 21:10:09 +0100

    Develope Class PrimeNumber

--- app/src/main/java/com/example/primeratarea/MainActivity.java
+++ app/src/main/java/com/example/primeratarea/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.primeratarea;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
@@ -9,9 +13,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    PrimeNumber primeNumber;
+    PrimeNumber primeNumber = new PrimeNumber();
+    EditText chosenNumber;
+    Button calcButton;
+    TextView result;
+    ArrayList numbersList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,39 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        chosenNumber = findViewById(R.id.editNumber);
+        calcButton = findViewById(R.id.calc);
+        result = findViewById(R.id.result);
+
+        //Button 'Calcular' event
+        calcButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                String input = chosenNumber.getText().toString();
+                int inputInt = 0;
+                int finalNumber = 0;
+
+                if (!input.isEmpty()){
+
+                    inputInt = Integer.parseInt(input);
+
+                    if(inputInt < 99999){
+                        finalNumber = primeNumber.calc(inputInt);
+                    }
+                }
+
+                if (inputInt == 0){
+                    result.setText("No has introducido ningún número");
+                } else if (inputInt > 99999){
+                    result.setText("El número no debe tener más de cinco dígitos");
+                } else {
+                    result.setText("El número primo nº " + input + " es el " + finalNumber);
+                }
+            }
         });
     }
 }
--- app/src/main/java/com/example/primeratarea/PrimeNumber.java
+++ app/src/main/java/com/example/primeratarea/PrimeNumber.java
@@ -1,6 +1,49 @@
 package com.example.primeratarea;
 
+import java.util.HashMap;
+
 public class PrimeNumber {
 
+    private static HashMap<Integer, Integer> calculatedNumbers =  new HashMap<>();
 
+    //Calc position prime number function
+    public int calc(int number){
+
+        if(calculatedNumbers.containsKey(number)){
+            return calculatedNumbers.get(number);
+        }
+
+        int primeCount = 1;
+        int verifyNumber = 1;
+
+        while (primeCount < number){
+
+            verifyNumber++;
+
+            if (isPrime(verifyNumber)){
+                primeCount++;
+            }
+        }
+
+        calculatedNumbers.put(number, verifyNumber);
+
+        return verifyNumber;
+    }
+
+    //Verify if is a prime number function
+    private static boolean isPrime(int n){
+
+        if(n <= 1){
+            return false;
+        }
+
+        for (int i = 2; i < n; i++){
+
+            if (n % i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
